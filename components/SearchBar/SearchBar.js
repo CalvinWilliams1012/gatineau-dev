73465dcc4902647459e614e3260f36c2d7546682
Calvin Williams <calvin.william1012@gmail.com>
2021-06-05 23:41:23 -0400

    moved gmap script load from head to on search bar component mount in order to create google latlong for autocomplete props

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class SearchBar extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -10,9 +11,14 @@ class SearchBar extends React.Component {
             latitude: null,
             longitude: null,
             isGeocoding: false,
+            gmapsLoaded: false,
           };
     }
 
+    /* 
+      Handle change from the PlacesAutocomplete when the user types their address.
+      Simply sets state.
+    */
     handleChange = (address) => {
         this.setState({
             address,
@@ -22,7 +28,25 @@ class SearchBar extends React.Component {
         })
     }
 
+    /* 
+      This will result in render() being called twice
+      however this is required due to the need to use google.maps.LatLang to create an object to send to PlacesAutocomplete prop
+      we load the google script at the end of the body as soon as the component mounts
+      then upon the js loading, the callback initGoog is called that sets gmapsLoaded, which we check in order to render PlacesAutocomplete.
 
+      Details: https://github.com/hibiken/react-places-autocomplete/issues/57
+
+      Not really optimal, as this is bad for performance..
+    */
+    componentDidMount(){
+      const { gmapsLoaded } = this.state;
+      if(gmapsLoaded === false){
+        window.initGoog = () => this.setState({gmapsLoaded: true});
+        const gmapScript = document.createElement(`script`)
+        gmapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API}&libraries=places&callback=initGoog`
+        document.querySelector(`body`).insertAdjacentElement(`beforeend`, gmapScript)
+      }
+    }
 
     render() {
         const {
@@ -30,42 +54,62 @@ class SearchBar extends React.Component {
             errorMessage,
             latitude,
             longitude,
-            isGeocoding
+            isGeocoding,
+            gmapsLoaded
         } = this.state;
 
+        /* 
+          Search options for the google api.
+          location = latitude and longitude of the city of Gatineau, in combination with 'radius' biases result to Gatineau.
+          componentRestrictions = restricts results to Canada.
+          radius = 30000 metres, in combination with 'location' biases result to Gatineau.
+          types = will only return 'address' types for predictions, as only addresses would have assessments.
+        */
         return (
           <div>
-            <PlacesAutocomplete
-              onChange={this.handleChange}
-              value={address}
-
-              shouldFetchSuggestions={address.length > 2}
-            >
-              {({
-                getInputProps,
-                suggestions,
-                getSuggestionItemProps,
-                loading,
-              }) => {
-                return (
-                  <div>
-                    <input
-                      {...getInputProps({ placeholder: "Enter Address..." })}
-                    />
+            {gmapsLoaded && (
+              <PlacesAutocomplete
+                onChange={this.handleChange}
+                value={address}
+                shouldFetchSuggestions={address.length > 3}
+                searchOptions={{
+                  location: new window.google.maps.LatLng(
+                    45.476543,
+                    -75.701271
+                  ),
+                  componentRestrictions: {
+                    country: "CA",
+                  },
+                  radius: 30000,
+                  types: ["address"],
+                }}
+              >
+                {({
+                  getInputProps,
+                  suggestions,
+                  getSuggestionItemProps,
+                  loading,
+                }) => {
+                  return (
                     <div>
+                      <input
+                        {...getInputProps({ placeholder: "Enter Address..." })}
+                      />
+                      <div>
                         {loading && <div>Loading...</div>}
-                        {suggestions.map(suggestion => {
-                            return (
-                                <div {...getSuggestionItemProps(suggestion)}>
-                                    <span>{suggestion.description}</span>
-                                </div>
-                            )
+                        {suggestions.map((suggestion) => {
+                          return (
+                            <div {...getSuggestionItemProps(suggestion)}>
+                              <span>{suggestion.description}</span>
+                            </div>
+                          );
                         })}
+                      </div>
                     </div>
-                  </div>
-                );
-              }}
-            </PlacesAutocomplete>
+                  );
+                }}
+              </PlacesAutocomplete>
+            )}
           </div>
         );
     }
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,6 @@ export default function Home() {
           rel="stylesheet"
         />
         <link rel="icon" href="/Logo.png" />
-        <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API}&libraries=places`}
-        ></script>
       </Head>
       <header className={styles.header}>
         <Image
