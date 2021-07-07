529aa3113f2658f5b6b0b85a7488305a5dcf5ef4
Calvin Williams <calvin.william1012@gmail.com>
2021-07-07 01:03:26 -0400

    make assessment data display address

--- components/AssessmentData/AssessmentData.js
+++ components/AssessmentData/AssessmentData.js
@@ -1,7 +1,18 @@
-const AssessmentData = (address) => {
+import React, { useState, useEffect } from 'react';
+
+function AssessmentData(props){
+    const [assessment,setAssessment] = useState('');
+
+    useEffect(() => {
+        setAssessment(props.address);
+        /*fetch(process.env.ASSESSMENT_API_URI + process.env.ASSESSMENT_API_PATH).then(
+            data => setAssessment(data.json())
+        )*/
+    }, [props.address])
+
     return (
         <div>
-            {console.log(address)}
+            {assessment}
         </div>
     )
 }
